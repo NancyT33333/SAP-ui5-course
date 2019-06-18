338a2bb69392751bea5c9df4d1c82c7772cb9020
Anna Tuzova <nancyt33333@gmail.com>
2019-06-18 10:20:42 +0000

    files changed

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,10 +1,12 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller" ,              // dependencies go here
-	"sap/m/MessageToast"
-], function (Controller, MessageToast) {
+	"sap/m/MessageToast",
+	"opensap/myapp/model/formatter"
+
+], function (Controller, MessageToast, formatter) {
 	"use strict";
 	return Controller.extend("opensap.myapp.controller.App", {
-
+		formatter : formatter, 
 		onShowHello : function () {
 					// read msg from i18n model
 			var oBundle = this.getView().getModel("i18n").getResourceBundle();
